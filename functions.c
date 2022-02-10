c784669391a55b96be15c02341792d66e4082330
Blake Perrin <blake.perrin13@gmail.com>
2022-02-09 23:08:35 -0600

    Merged my code with yours

--- functions.c
+++ functions.c
@@ -53,3 +53,72 @@ char keyword_check (char input[], char *ptr) {
 	return 0;
 }
 
+char execute_command (char input[], char debug) {
+
+	int params = 0;
+	char toggle = 0;
+	char* input2 = strdup(input);
+	char* token = strtok(input, " ");
+	
+	if (debug)
+		printf("Arguments: ");
+	while(token != NULL) {
+         	params += 1;
+         	if (debug)
+         		printf("%s ", token);
+         	token = strtok(NULL, " ");
+      	}
+      		
+      	if (debug)
+      		printf("\nNumber of Arguments: %d ", params);
+      		
+      	char* argument_list[params];
+		
+	token = strtok(input2, " ");
+			
+	if (debug)
+		printf("\nArgument List: ");
+    	for(int i = 0; i < params + 1; i++) {
+        	argument_list[i] = token;
+        	if (debug)
+    			printf("%s ", token);
+        	token = strtok(NULL, " ");
+    	}
+    			
+	int status;
+			
+	pid_t pid = fork();
+			
+	// if we need the PID of the Child
+    	pid_t child = getpid();
+
+		
+	// if in child process
+	if(pid == 0){
+		// run input inside child process bro ...
+        	if (debug) 
+        		printf("\nIn Child: %d\n", child);
+        	toggle = 1;
+        	fflush(stdout);
+        	int status_code = execvp(argument_list[0], argument_list);
+        	
+        	if (status_code == -1) {
+        		printf("%s: command not found\n", input);
+        		exit(100);
+        	}
+	}	
+        // if in parent process
+    	else {
+    		toggle = 1;
+		wait(NULL);
+    	}
+    	return 1;
+}
+
+
+
+
+
+
+
+
